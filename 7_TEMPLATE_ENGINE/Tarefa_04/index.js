7e1ad7dace9fac024deadcdd5bbe07a3f1b87c56
Gustavo <gustavoog05@hotmail.com>
2025-03-19 17:48:35 -0300

    desenvolvendo lógicas do login e senha

--- 7_TEMPLATE_ENGINE/Tarefa_04/index.js
+++ 7_TEMPLATE_ENGINE/Tarefa_04/index.js
@@ -1,5 +1,7 @@
+const fs = require('fs')
 const express = require('express')
 const handlebars = require('express-handlebars')
+const { register } = require('module')
 const app = express()
 const port = 3000
 let user
@@ -17,9 +19,34 @@ app.post('/register', (req, res) => {
     usersurname = req.body.surname
     userpassword = req.body.password
     userpasswordconf = req.body.confirmpassword
+    registerPage = fs.readFileSync('/views/register.html')
+
+    if(verifyAccount && username.length > 0){
+        if(userpassword == userpasswordconf){
+            //criar json da conta
+            let accountData = {
+                name: username,
+                usersurname: usersurname,
+                userpassword: userpassword
+            }
+            createAccount(accountData)
+            registerPage.replace('<h3 id="warning"></h3>', '<h3 id="warning" Style="text-align: center; color: green;">Seu registro foi efetuado com sucesso</h3>')
+        }
+        else{
+            registerPage.replace('<h3 id="warning"></h3>', '<h3 id="warning" Style="text-align: center; color: red;">As senhas não são iguais</h3>')
+        }
+    }
+    else{
+        registerPage.replace('<h3 id="warning"></h3>', '<h3 id="warning" Style="text-align: center; color: red;">Ocorreu um erro ao criar a conta</h3>')
+    }
+
 })
 
 app.get('/login', (req, res) => {
+    username = req.body.name
+    password = req.body.password
+
+
     res.render('login')
 })
 
@@ -31,6 +58,34 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.use(function(req, res, next){
+    res.status(404).sendFile(`${basePath}/404.html`)
+})
+
 app.listen(port, () => {
     console.log("Servidor ativo")
 })
+
+function verifyAccount(accountName){
+    if(!fs.existsSync('accounts')){
+        fs.mkdirSync('accounts')
+    }
+    if(fs.existsSync(`/accounts/${accountName}`)){
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: "utf8",
+        flag: "r"
+    })
+    return JSON.parse(accountJSON)
+}
+
+function createAccount(accountData){
+    fs.writeFileSync(`accounts/${accountData.name}.json`, `{\n"surname": "${accountData.surname}",\n "password": "${accountData.password}"}`)
+}
